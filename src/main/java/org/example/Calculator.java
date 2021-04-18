12c5453aff40491ef3aade74c1db0698d9ac2817
Majid <Mansouridb@gmail.com>
2021-04-18 16:09:58 +0200

    extract methods reformat codes add iff else condition for quit

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,57 +1,105 @@
 package org.example;
 
-import java.util.Scanner;
+import java.util.*;
+
 public class Calculator {
+    private static Object MessageBoxBYesNo;
+//Method for Multiply numbers,
 
-            public static void main(String[] args) {
+    public static void multiply(double num1, double num2) {
+        double result = num1 * num2;
 
-                char operator;
-                double num1, num2, result;
+        System.out.println(num1 + " * " + num2 + " = " + result);
+    }
 
-                Scanner input = new Scanner(System.in);
+    // Method for Division numbers
 
-                System.out.println("Select one of them: *, /, +, -, ");
-                operator = input.next().charAt(0);
+    public static void division(double num1, double num2) {
+        double result = num1 / num2;
+        System.out.println(num1 + "/" + num2 + "=" + result);
+    }
+// Method for subtract numbers,
 
+    public static void subtract(double num1, double num2) {
+        double result = num1 - num2;
+        System.out.println(num1 + "-" + num2 + "=" + result);
 
-                System.out.println("num1");
-                num1 = input.nextDouble();
+    }
+//Method for  Sum numbers,
 
-                System.out.println("num2");
-                num2 = input.nextDouble();
-
-                switch (operator) {
-                    case '*':
-                        result = num1 * num2;
-                        System.out.println(num1 + " * " + num2 + " = " + result);
-                        break;
-
-
-                    case '/':
-                        result = num1 / num2;
-                        System.out.println(num1 + " / " + num2 + " = " + result);
-                        break;
-
-                   case '+':
-                        result = num1 + num2;
-                        System.out.println(num1 + " + " + num2 + " = " + result);
-                        break;
+    public static void sum(double num1, double num2) {
+        double result = num1 + num2;
+        System.out.println(num1 + "+" + num2 + "=" + result);
 
 
-                    case '-':
-                        result = num1 - num2;
-                        System.out.println(num1 + " - " + num2 + " = " + result);
-                        break;
+    }
+
+    public static void main(String[] args) {
+
+        char operator;
+
+        double num1, num2, result;
+        boolean quit = true;
+
+        while (quit) {
 
 
-                    default:
-                        System.out.println("Wrong !");
-                        break;
-                }
-
-                input.close();
+            Scanner input = new Scanner(System.in);
+            System.out.println("Please enter the first number");
+            num1 = input.nextDouble();
+            System.out.println("Please enter the second  number");
+            num2 = input.nextDouble();
+            System.out.println("Select one of them: *, /, +, -" + " ");
+            operator = input.next().charAt(0);
+            if (!(operator == '+' || operator == '-' || operator == '*' || operator == '/')) {
+                System.err.print("Invalid operator please use only + or - or / *");
             }
+
+            switch (operator) {
+                case '*':
+                    multiply(num1, num2);
+
+                    break;
+
+
+                case '/':
+                    division(num1, num2);
+
+                    break;
+
+                case '+':
+                    sum(num1, num2);
+                    break;
+
+                case '-':
+                    subtract(num1, num2);
+
+                    break;
+
+                default:
+                    System.out.println("Wrong !");
+                    break;
+            }
+
+            // input.close();
+
+
+            System.out.println(" Do you want to use Calculator, Y/N ?");
+            char check = input.next().charAt(0);
+            if (check == 'y') {
+                quit = true;
+            } else {
+                quit = false;
+            }
+
+
+//input.close;
+
         }
+
+    }
+}
+
 
 
 
